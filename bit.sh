f2787bb62fcbe6ad7d40463b4f45443da40b3c53
sharp421 <83478281+sharp421@users.noreply.github.com>
2021-05-15 09:53:31 +0700

    Update bit.sh

--- bit.sh
+++ bit.sh
@@ -1,14 +1,4 @@
-apt install wget 
-apt install screen
-apt install proot -y
-apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y 
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y
-apt-get install -qqy automake
-apt-get install -qqy libcurl4-openssl-dev
-apt-get install -qqy make
-git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2.git
-cd wolf-m7m-cpuminer-V2
-./autogen.sh
-./configure CFLAGS="-O3"
-make
-./minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DKFuAREo755smianiJTuUEwt5wF9rAKPCe -p c=DGB -t 6
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DKFuAREo755smianiJTuUEwt5wF9rAKPCe -p c=DGB,zap=PYRK-lyra2z330 -t 6
