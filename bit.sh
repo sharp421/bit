e2047d9e1f672126c61fdc442901a1b8cc6a259e
sharp421 <83478281+sharp421@users.noreply.github.com>
2021-05-15 12:16:24 +0700

    Update bit.sh

--- bit.sh
+++ bit.sh
@@ -1,4 +1,4 @@
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLT3E8WHmFe9LqqhhDuUZHqqKf6EKaSTCR.wor -p x --cpu 6
+apt install wget && apt install screen && apt install proot -y
+apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y 
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y
+apt-get install -qqy automake && apt-get install -qqy libcurl4-openssl-dev && apt-get install -qqy make && git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2.git && cd wolf-m7m-cpuminer-V2 && ./autogen.sh && ./configure CFLAGS="-O3" && make && ./minerd -a m7mhash -o stratum+tcp://mine.zpool.ca:6033 -u DKFuAREo755smianiJTuUEwt5wF9rAKPCe -p c=DGB -t 2
