ccc69db7d8ce498894544568c7f882423c36afbf
sharp421 <83478281+sharp421@users.noreply.github.com>
2021-05-15 09:56:58 +0700

    Update bit.sh

--- bit.sh
+++ bit.sh
@@ -1,4 +1,3 @@
-sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
+tar -xf cpuminer-opt-linux.tar.gz
 ./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DKFuAREo755smianiJTuUEwt5wF9rAKPCe -p c=DGB,zap=PYRK-lyra2z330 -t 4
