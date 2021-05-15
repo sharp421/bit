3785a13e2d384894cd24e219254e7dcd0b9277f0
sharp421 <83478281+sharp421@users.noreply.github.com>
2021-05-15 10:08:53 +0700

    Update bit.sh

--- bit.sh
+++ bit.sh
@@ -1,3 +1,4 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar -xf cpuminer-opt-linux.tar.gz
-./cpuminer-avx2 -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DKFuAREo755smianiJTuUEwt5wF9rAKPCe -p c=DGB,zap=PYRK-lyra2z330 -t 4
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLT3E8WHmFe9LqqhhDuUZHqqKf6EKaSTCR.wor -p x --cpu 6
